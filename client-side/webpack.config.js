524d6816194c1676a92573510da3952c8e8c3a3b
Nikolaj Lozitski <Soullnik96@gmail.com>
2021-01-11 12:57:03 +0300

    feat: change webpack plugin

--- client-side/webpack.config.js
+++ client-side/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = {
   entry: "./src/main.tsx",
   target: "web",
   mode: "development",
-  entry: ["@babel/polyfill", "./src/main.tsx"],
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
@@ -20,6 +22,7 @@ module.exports = {
       {
         test: /\.(ts|tsx)$/,
         loader: "awesome-typescript-loader",
+        exclude: /node_modules/,
         options: {
           useBabel: true,
           babelCore: "@babel/core",
@@ -29,20 +32,34 @@ module.exports = {
         enforce: "pre",
         test: /\.js$/,
         loader: "source-map-loader",
+        exclude: /node_modules/
       },
       {
         test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|ico)$/,
         use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader",
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: 'images',
+              name: '[name]-[sha1:hash:7].[ext]'
+            }
+          },
         ],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
+      buildTime: new Date().toDateString(),
       template: path.resolve(__dirname, "public", "index.html"),
-    })
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main-[hash:8].css'
+    }),
+    new BundleAnalyzerPlugin()
   ],
 };
