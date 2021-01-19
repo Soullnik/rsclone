4ba33258f9769fadeeab952183d12df94f00d0df
Nikolaj Lozitski <Soullnik96@gmail.com>
2021-01-19 09:26:13 +0300

    feat: add lazy loading component

--- client-side/src/app/index.tsx
+++ client-side/src/app/index.tsx
@@ -1,38 +1,42 @@
-import React, { Suspense } from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { ConnectedRouter } from 'connected-react-router';
+import React, { Suspense, lazy } from 'react';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+// import { ConnectedRouter } from 'connected-react-router';
 import { Provider } from 'react-redux';
-import store, { history } from '../redux/store';
+import { store } from '../redux/store';
 import { Spin, Layout } from 'antd';
 import { Header, Footer, Side } from '../layout';
-import { Home, Messenger, News, Profile } from '../pages';
+
+const Home = lazy(() => import('../pages/home'));
+const Messenger = lazy(() => import('../pages/messenger'));
+const News = lazy(() => import('../pages/news'));
+const Profile = lazy(() => import('../pages/profile'));
 
 const App = () => {
   const { Content } = Layout;
 
   return (
     <Provider store={store}>
-      <Suspense fallback={<Spin size="large" style={{ margin: 'auto' }} />}>
-        <BrowserRouter basename="/">
-          <ConnectedRouter history={history}>
-            <Layout style={{ minHeight: '100vh' }}>
-              <Side />
-              <Layout className="site-layout">
-                <Header />
-                <Content style={{ margin: '10px 16px' }}>
-                  <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/messenger" component={Messenger} />
-                    <Route exact path="/news" component={News} />
-                    <Route exact path="/profile" component={Profile} />
-                  </Switch>
-                </Content>
-                <Footer />
-              </Layout>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Suspense fallback={<Spin size="large" style={{ margin: 'auto' }} />}>
+          <Router basename="/">
+            <Side />
+            <Layout className="site-layout">
+              <Header />
+              <Content style={{ margin: '10px 16px' }}>
+                <Switch>
+                  {/* <Route path="/sign-in" render={() => <SignIn />} />
+                  <Route path="/sign-up" render={() => <SignUp />} /> */}
+                  <Route name="massenger" exect path="/messenger" component={(props: JSX.IntrinsicAttributes & { children?: React.ReactNode; }) => <Messenger {...props} />} />
+                  <Route path="/news" render={() => <News />} />
+                  <Route path="/profile" render={() => <Profile />} />
+                  <Route exact path="/" render={() => <Home />} />
+                </Switch>
+              </Content>
+              <Footer />
             </Layout>
-          </ConnectedRouter>
-        </BrowserRouter>
-      </Suspense>
+          </Router>
+        </Suspense>
+      </Layout>
     </Provider>
   );
 };
--- client-side/src/redux/store.tsx
+++ client-side/src/redux/store.tsx
@@ -1,20 +1,29 @@
-import { createBrowserHistory, History } from 'history';
-import { routerMiddleware } from 'connected-react-router';
-import { createStore, applyMiddleware, compose } from 'redux';
+// import { createBrowserHistory, History } from 'history';
+// import { routerMiddleware } from 'connected-react-router';
+// import { createStore, applyMiddleware, compose } from 'redux';
 
-import createSagaMiddleware from 'redux-saga';
+// import createSagaMiddleware from 'redux-saga';
 
-import createRootReducer from './reducers';
-import sagas from './sagas';
+// import createRootReducer from './reducers';
+// import sagas from './sagas';
 
-const history: History = createBrowserHistory();
-const sagaMiddleware = createSagaMiddleware();
-const middleware = [routerMiddleware(history), sagaMiddleware];
+// const history: History = createBrowserHistory();
+// const sagaMiddleware = createSagaMiddleware();
+// const middleware = [routerMiddleware(history), sagaMiddleware];
 
-const store = createStore(createRootReducer(history), compose(applyMiddleware(...middleware)));
+// const store = createStore(createRootReducer(history), compose(applyMiddleware(...middleware)));
 
-sagaMiddleware.run(sagas);
+// sagaMiddleware.run(sagas);
 
-export { history };
+// export { history };
 
-export default store;
+// export default store;
+
+import { createStore, compose, applyMiddleware } from 'redux';
+import { rootReducer } from './reducers';
+
+const initialState = {};
+const enhancers: any[] = [];
+const composedEnhancers = compose(applyMiddleware(), ...enhancers);
+
+export const store = createStore(rootReducer, initialState, composedEnhancers);
