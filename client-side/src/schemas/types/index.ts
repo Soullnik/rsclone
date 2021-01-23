72c5136f260eb62f88f099bcb717dac4cd885ea4
Nikolaj Lozitski <Soullnik96@gmail.com>
2021-01-24 00:07:56 +0300

    feat: refactoring schemas

---
+++ client-side/src/schemas/types/index.ts
@@ -0,0 +1,5 @@
+import type { TypeUser } from './user';
+import type { IReducerStates } from './reducerStates';
+import type { TypeLoginArguments } from './login';
+
+export type { TypeUser, TypeLoginArguments, IReducerStates };
